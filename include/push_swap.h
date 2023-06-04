227550596b9e85bee6e31625c9c56fa447cd68d3
Léo <leogaudinoff@gmail.com>
2023-06-04 12:19:46 +0200

    build: add main header

---
+++ include/push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 11:58:50 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/06/04 12:14:34 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include "ft_printf/libft/libft.h"
+#include "ft_printf/ft_printf.h"
+
+#endif
