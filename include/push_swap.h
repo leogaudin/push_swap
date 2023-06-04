3833e7fd8b633f3f53bce54501dbfa83f6f789ad
Léo <leogaudinoff@gmail.com>
2023-06-04 12:42:44 +0200

    refactor: format to comply with Norme

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,14 +6,14 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 11:58:50 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/06/04 12:14:34 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/06/04 12:42:20 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include "ft_printf/libft/libft.h"
-#include "ft_printf/ft_printf.h"
+# include "ft_printf/ft_printf.h"
+# include "ft_printf/libft/libft.h"
 
 #endif
