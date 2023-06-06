8e3038f884f0c33cd005938fc535f7d56f875b38
Léo <leogaudinoff@gmail.com>
2023-06-06 15:03:06 +0200

    feat: implement successful sort (unoptimised)

--- src/stack/sort.c
+++ src/stack/sort.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 15:48:14 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/06/06 13:04:51 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/06/06 15:02:48 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,51 +46,67 @@ static int	get_min_cost(int a, int b)
 {
 	if (a <= b)
 		return (1);
-	if (a == -1 && b != -1)
+	else if (a == -1 && b != -1)
 		return (-1);
 	return (2);
 }
 
-static void	move_in_range_to_top(t_stack *stack, int min, int max)
+static void	move_in_range_to_top(t_stack *stack, int min, int max, char stack_name)
 {
 	int	closest;
-	int	count;
-	int i;
+	int top_scan;
+	int bottom_scan;
 
-	closest = get_min_cost(
-		stack->size - scan_from_top(stack, min, max),
-		scan_from_bottom(stack, min, max)
-	);
-	i = 0;
+	top_scan = stack->top - scan_from_top(stack, min, max);
+	bottom_scan = scan_from_bottom(stack, min, max) + 1;
+	closest = get_min_cost(top_scan, bottom_scan);
 	if (closest == 1)
 	{
-		count = scan_from_top(stack, min, max);
-		while (i++ < count)
-			ra(stack);
+		while (scan_from_bottom(stack, min, max) + 1 > 0 && stack->top - scan_from_top(stack, min, max) > 0)
+		{
+			if (stack_name == 'a')
+				ra(stack);
+			else
+				rb(stack);
+		}
 	}
 	else if (closest == 2)
 	{
-		count = scan_from_bottom(stack, min, max);
-		while (i++ < count)
-			rra(stack);
+		while (scan_from_bottom(stack, min, max) + 1 > 0 && stack->top - scan_from_top(stack, min, max) > 0)
+		{
+			if (stack_name == 'a')
+				rra(stack);
+			else
+				rrb(stack);
+		}
 	}
 }
 
 static void	check_b_and_move(t_stack *stack_a, t_stack *stack_b, int value)
 {
-	move_in_range_to_top(stack_b, -2147483647,
-			stack_b->pile[value]);
-	pb(stack_a, stack_b);
+	if (value + 1 == value + 2 - 1)
+		pb(stack_a, stack_b);
+}
+
+static void dump_back(t_stack *stack_a, t_stack *stack_b)
+{
+	int max;
+	while (stack_b->top >= 0)
+	{
+		max = find_max_index(stack_b);
+		move_in_range_to_top(stack_b, stack_b->pile[max],
+				stack_b->pile[max], 'b');
+		pa(stack_a, stack_b);
+	}
 }
 
 static void	sort_chunk(t_stack *stack_a, t_stack *stack_b, int chunk_start,
 		int chunk_end)
 {
-	// ft_printf("Sorting chunk with values %d - %d\n", chunk_start, chunk_end);
 	while (scan_from_top(stack_a, chunk_start, chunk_end) != -1
 		&& scan_from_bottom(stack_a, chunk_start, chunk_end) != -1)
 	{
-		move_in_range_to_top(stack_a, chunk_start, chunk_end);
+		move_in_range_to_top(stack_a, chunk_start, chunk_end, 'a');
 		check_b_and_move(stack_a, stack_b, stack_a->pile[stack_a->top]);
 	}
 }
@@ -111,12 +127,7 @@ static void big_sort(t_stack *stack_a, t_stack *stack_b)
 		sort_chunk(stack_a, stack_b, chunk_start, chunk_end);
 		chunk_start += chunk_size;
 	}
-	// while (stack_b->top >= 0)
-	// {
-	// 	move_in_range_to_top(stack_b, stack_b->pile[find_max_index(stack_b)],
-	// 			stack_b->pile[find_max_index(stack_b)]);
-	// 	pa(stack_a, stack_b);
-	// }
+	dump_back(stack_a, stack_b);
 }
 
 void	sort_stack(t_stack *stack_a, t_stack *stack_b)
@@ -130,28 +141,3 @@ void	sort_stack(t_stack *stack_a, t_stack *stack_b)
 	}
 	big_sort(stack_a, stack_b);
 }
-
-// void	sort_stack(t_stack *stack_a, t_stack *stack_b)
-// {
-// 	int	current;
-// 	int	previous;
-
-// 	if (stack_a->top <= 0)
-// 		return ;
-// 	while (stack_a->top > 0)
-// 	{
-// 		current = stack_a->pile[stack_a->top];
-// 		previous = stack_a->pile[stack_a->top - 1];
-// 		if (current > previous)
-// 		{
-// 			sa(stack_a);
-// 			if (stack_b->top >= 0
-// 				&& stack_b->pile[stack_b->top] > stack_a->pile[stack_a->top])
-// 				pa(stack_a, stack_b);
-// 		}
-// 		else
-// 			pb(stack_a, stack_b);
-// 	}
-// 	while (stack_b->top >= 0)
-// 		pa(stack_a, stack_b);
-// }
